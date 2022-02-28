11f7d50e6d4098002bc1068e9ad4015fe8aec5c9
Seow Li Chong Glen <glenseowlc@hotmail.com>
2022-03-01 02:56:30 +0800

    refactor frontend logic to use newly updated idl file, also solved annoying errors

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
 import { Program, Provider, web3 } from '@project-serum/anchor';
 
 import idl from './idl.json';
-import kp from './keypair.json';
 
 // SystemProgram is a reference to the Solana runtime!
-const { SystemProgram } = web3;
-
-// Create a keypair for the account that will hold the GIF data.
-const arr = Object.values(kp._keypair.secretKey);
-const secret = new Uint8Array(arr);
-const baseAccount = web3.Keypair.fromSecretKey(secret);
+const { SystemProgram, Keypair } = web3;
 
 // Get our program's id from the IDL file.
 const programID = new PublicKey(idl.metadata.address);
@@ -27,9 +20,6 @@ const opts = {
 }
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-
 const TEST_GIFS = [
 	'https://media.giphy.com/media/otnqsqqzmsw7K/giphy.gif',
 	'https://media.giphy.com/media/GbMEqTy3M5NMA/giphy.gif',
@@ -38,94 +28,32 @@ const TEST_GIFS = [
 ]
 
 const App = () => {
+  /*              STATE               */
 
   const [walletAddress, setWalletAddress] = useState(null);
   const [inputValue, setInputValue] = useState('');
   const [gifList, setGifList] = useState([]);
   const [hoveredGifIndex, setHoveredGifIndex] = useState(0);
-  const [loading, setLoading] = useState(false);
 
-  const checkIfWalletIsConnected = async () => {
-    try {
-      const { solana } = window;
-
-      if (solana) {
-        if (solana.isPhantom) {
-          console.log('Phantom wallet found!');
-          const response = await solana.connect({ onlyIfTrusted: true });
-          console.log(
-            'Connected with Public Key:',
-            response.publicKey.toString()
-          );
-          setWalletAddress(response.publicKey.toString());
-        }
-      } else {
-        alert('Solana object not found! Get a Phantom Wallet 👻');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const connectWallet = async () => {
-    const { solana } = window;
-  
-    if (solana) {
-      const response = await solana.connect();
-      console.log('Connected with Public Key:', response.publicKey.toString());
-      setWalletAddress(response.publicKey.toString());
-    }
-  };
-
-  const sendGif = async () => {
-    if (inputValue.length === 0) {
-      console.log("No gif link given!")
-      return
-    }
-    setInputValue('');
-    console.log('Gif link:', inputValue);
-    try {
-      const provider = getProvider();
-      const program = new Program(idl, programID, provider);
-  
-      await program.rpc.addGif(inputValue, {
-        accounts: {
-          baseAccount: baseAccount.publicKey,
-          user: provider.wallet.publicKey,
-        },
-      });
-      console.log("GIF successfully sent to program", inputValue)
-  
-      await getGifList();
-    } catch (error) {
-      console.log("Error sending GIF:", error)
-    }
-  };
+  /*              ACTIONS             */
 
   const onInputChange = (event) => {
     const { value } = event.target;
     setInputValue(value);
   };
 
-  const getProvider = () => {
-    const connection = new Connection(network, opts.preflightCommitment);
-    const provider = new Provider(
-      connection, window.solana, opts.preflightCommitment,
-    );
-    return provider;
-  }
-
-  let modal = document.getElementById("myModal");
-
   const renderModal = () => {
+    let modal = document.getElementById("myModal");
     modal.style.display = "block";
   }
 
   const renderCloseModalOnClick = () => {
+    let modal = document.getElementById("myModal");
     modal.style.display = "none";
   }
 
   window.onclick = function(event) {
+    let modal = document.getElementById("myModal");
     if (event.target === modal) {
       modal.style.display = "none";
     }
@@ -177,89 +105,136 @@ const App = () => {
   );
 
   const renderConnectedContainer = () => {
-    if (gifList === null) {
-      return (
-        <div className="connected-container">
-          <button className="cta-button submit-gif-button" onClick={createGifAccount}>
-            Do One-Time Initialization For GIF Program Account
-          </button>
-        </div>
-      )
-    } else {
-      return (
-        <div className="connected-container">
-          <form
-            onSubmit={(event) => {
-              event.preventDefault();
-              sendGif();
-            }}
-          >
-            <input
-              type="text"
-              placeholder="Submit a GIF link to the portal!"
-              value={inputValue}
-              onChange={onInputChange}
-            />
-            <button type="submit" className="cta-button submit-gif-button">Submit</button>
-          </form>
-          <div className="gif-grid">
-            {gifList.map((item, index) => (
-              <div 
-                className="gif-item" 
-                key={index} 
-                onClick={() => renderModal()} 
-                onMouseEnter={() => setHoveredGifIndex(index)}
-              >
-                <img src={item.gifLink} alt={item.gifLink} />
-                <div className="gif-description">
-                  <div className="text">🤍 0</div>
-                  <div className="text">💬 0</div>
-                </div>
+    return (
+      <div className="connected-container">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            sendGif();
+          }}
+        >
+          <input
+            type="text"
+            placeholder="Submit a GIF link to the portal!"
+            value={inputValue}
+            onChange={onInputChange}
+          />
+          <button type="submit" className="cta-button submit-gif-button">Submit</button>
+        </form>
+        <div className="gif-grid">
+          {gifList.map((item, index) => (
+            <div 
+              className="gif-item" 
+              key={index} 
+              onClick={() => renderModal()} 
+              onMouseEnter={() => setHoveredGifIndex(index)}
+            >
+              <img src={item.account.gifLink} alt={item.account.gifLink} />
+              <div className="gif-description">
+                <div className="text">🤍 0</div>
+                <div className="text">💬 0</div>
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      )
+      </div>
+      
+    )
+  };
+
+  /*             APIs                    */
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const createGifAccount = async () => {
+  const connectWallet = async () => {
+    const { solana } = window;
+  
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection, window.solana, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  const sendGif = async () => {
+    if (inputValue.length === 0) {
+      console.log("No gif link given!")
+      return
+    }
+    setInputValue('');
+    console.log('Gif link:', inputValue);
+    console.log('Hovered Gif: ', hoveredGifIndex);
     try {
       const provider = getProvider();
       const program = new Program(idl, programID, provider);
-      console.log("ping")
-      await program.rpc.startStuffOff({
+      const gifKeypair = Keypair.generate();
+
+      await program.rpc.addGif(inputValue, {
         accounts: {
-          baseAccount: baseAccount.publicKey,
+          gif: gifKeypair.publicKey,
           user: provider.wallet.publicKey,
           systemProgram: SystemProgram.programId,
         },
-        signers: [baseAccount]
+        signers: [gifKeypair],
       });
-      console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
-      await getGifList();
-  
-    } catch(error) {
-      console.log("Error creating BaseAccount account:", error)
+      console.log("GIF successfully sent to program", inputValue);
+
+      const account = await program.account.gif.fetch(gifKeypair.publicKey);
+      console.log(account);
+      setGifList(gifList => [
+        {
+          publicKey: gifKeypair.publicKey,
+          account: account,
+        },
+        ...gifList, 
+      ]);
+    } catch (error) {
+      console.log("Error sending GIF:", error)
     }
-  }
+  };
 
   const getGifList = async() => {
-    setLoading(true);
     try {
       const provider = getProvider();
       const program = new Program(idl, programID, provider);
-      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-      
-      console.log("Got the account", account)
-      setGifList(account.gifList.reverse())
-      setLoading(false);
+      const accounts = await program.account.gif.all();
+      console.log("list of gif accounts: ", accounts);
+      setGifList(accounts);   
     } catch (error) {
       console.log("Error in getGifList: ", error)
       setGifList(null);
-      setLoading(false);
     }
   }
+
+  /*              useEffects             */
 
   useEffect(() => {
     const onLoad = async () => {
@@ -296,25 +271,25 @@ const App = () => {
                     <div className="modal-image-container">
                       <img 
                         className="modal-image" 
-                        src={gifList[hoveredGifIndex].gifLink} 
-                        alt={gifList[hoveredGifIndex].gifLink}
+                        src={gifList[hoveredGifIndex].account.gifLink} 
+                        alt={gifList[hoveredGifIndex].account.gifLink}
                       ></img>
                     </div>
                     <div className="modal-actions-container">
                       <div className="title-container">
                         <div className="title">GIF by: <strong>{
-                          gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
+                          gifList[hoveredGifIndex].account.user.toString().slice(0, 4) 
                           + '...' 
-                          + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
+                          + gifList[hoveredGifIndex].account.user.toString().slice(gifList[hoveredGifIndex].account.user.toString().length - 4,)
                         }</strong> ({formatDate(new Date())})</div>
                       </div>
                       <div className="comments-container">
                         <div className="single-comment-container">
                           <div className="single-comment">
                             <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
+                              gifList[hoveredGifIndex].account.user.toString().slice(0, 4) 
                               + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
+                              + gifList[hoveredGifIndex].account.user.toString().slice(gifList[hoveredGifIndex].account.user.toString().length - 4,)
                             }</div>
                             {": hello!!! test test test test test test \n\ntest test test test test test test test test test \ntest test test test test  test test test test"} 
                           </div>
@@ -325,9 +300,9 @@ const App = () => {
                         <div className="single-comment-container">
                           <div className="single-comment">
                             <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
+                              gifList[hoveredGifIndex].account.user.toString().slice(0, 4) 
                               + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
+                              + gifList[hoveredGifIndex].account.user.toString().slice(gifList[hoveredGifIndex].account.user.toString().length - 4,)
                             }</div>
                             : hello how are you i am good thanks
                           </div>
@@ -335,86 +310,6 @@ const App = () => {
                             {formatDate(new Date())}
                           </div>
                         </div>
-                        {/* START OF OVERFLOW TESTING */}
-                        <div className="single-comment-container">
-                          <div className="single-comment">
-                            <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
-                              + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
-                            }</div>
-                            {": hello!!! test test test test test test \n\ntest test test test test test test test test test \ntest test test test test  test test test test"} 
-                          </div>
-                          <div className="comment-timestamp">
-                            {formatDate(new Date())}
-                          </div>
-                        </div>
-                        <div className="single-comment-container">
-                          <div className="single-comment">
-                            <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
-                              + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
-                            }</div>
-                            {": hello!!! test test test test test test \n\ntest test test test test test test test test test \ntest test test test test  test test test test"} 
-                          </div>
-                          <div className="comment-timestamp">
-                            {formatDate(new Date())}
-                          </div>
-                        </div>
-                        <div className="single-comment-container">
-                          <div className="single-comment">
-                            <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
-                              + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
-                            }</div>
-                            {": hello!!! test test test test test test \n\ntest test test test test test test test test test \ntest test test test test  test test test test"} 
-                          </div>
-                          <div className="comment-timestamp">
-                            {formatDate(new Date())}
-                          </div>
-                        </div>
-                        <div className="single-comment-container">
-                          <div className="single-comment">
-                            <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
-                              + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
-                            }</div>
-                            {": hello!!! test test test test test test \n\ntest test test test test test test test test test \ntest test test test test  test test test test"} 
-                          </div>
-                          <div className="comment-timestamp">
-                            {formatDate(new Date())}
-                          </div>
-                        </div>
-                        <div className="single-comment-container">
-                          <div className="single-comment">
-                            <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
-                              + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
-                            }</div>
-                            {": hello!!! test test test test test test \n\ntest test test test test test test test test test \ntest test test test test  test test test test"} 
-                          </div>
-                          <div className="comment-timestamp">
-                            {formatDate(new Date())}
-                          </div>
-                        </div>
-                        <div className="single-comment-container">
-                          <div className="single-comment">
-                            <div className="single-comment-wallet-addr">{
-                              gifList[hoveredGifIndex].userAddress.toString().slice(0, 4) 
-                              + '...' 
-                              + gifList[hoveredGifIndex].userAddress.toString().slice(gifList[hoveredGifIndex].userAddress.toString().length - 4,)
-                            }</div>
-                            {": hello!!! test test test test test test \n\ntest test test test test test test test test test \ntest test test test test  test test test test"} 
-                          </div>
-                          <div className="comment-timestamp">
-                            {formatDate(new Date())}
-                          </div>
-                        </div>
-                        {/* END OF OVERFLOW TESTING */}
                       </div>
                       <div className="operations-container">
                         <div className="like-and-tips-container">
@@ -472,15 +367,6 @@ const App = () => {
         </div>
         {!walletAddress && renderNotConnectedContainer()}
         {walletAddress && gifList && renderConnectedContainer()}
-        <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-          <a
-            className="footer-text"
-            href={TWITTER_LINK}
-            target="_blank"
-            rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
-        </div>
       </div>
     </div>
   );
